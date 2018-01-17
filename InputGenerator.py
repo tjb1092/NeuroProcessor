e5f23e70f6872b17468b76ed1a8f0e592c3a9b96
tonyjb <baileytj@mail.uc.edu>
2018-01-17 17:11:33 -0500

    Parsed data files for Tony and Jessica

--- InputGenerator.py
+++ InputGenerator.py
@@ -1,4 +1,11 @@
 from pydub import AudioSegment
+import os
+import tkinter
+#dir = os.path.dirname(__file__)
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename()
+tkinter.Tk().withdraw()
+#AudioFolder = os.path.join(dir, '/relative/path/to/file/you/want')
 
 sound1 = AudioSegment.from_wav("amy.wav")
 sound2 = AudioSegment.from_wav("eric.wav")
@@ -6,7 +13,6 @@ sound2 = AudioSegment.from_wav("eric.wav")
 combined_sounds = sound1 + sound2 * 2
 
 combined_sounds.export("monstrosity.wav")
-
 
 #def TestStruct():
 #    def __init__:
