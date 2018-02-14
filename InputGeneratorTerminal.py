9ed5555842ff7ffc6855ff162c60b39d4f4dc9c5
tonyjb <baileytj@mail.uc.edu>
2018-02-14 13:41:05 -0500

    Modularized the main code and utilized this to implement a single voice randomization w/ labels

--- InputGeneratorTerminal.py
+++ InputGeneratorTerminal.py
@@ -6,50 +6,69 @@ import math
 import pickle
 from SpiceModifier import GenerateLabels
 from FindVoiceTiming import voiceTimings
-def Random_Random(v1, v2, audio):
-    Train_Split_Percent = 0.8
 
-    ############################################################################
+def Random_Random(v1, v2, audio, Train_Split_Percent):
+
     # Get list of V1 files & create list data structure
+    V1, V1_trainLen, V1_testLen = getV(v1, audio, Train_Split_Percent, 1)
 
-    # Iterate through each file set in the walk and get a list of all of the audio files
-    V1 = []
-    for dirpath, dirs, files in os.walk(os.path.join(audio, v1)):
-        for afile in files:
-            V1.append((os.path.join(audio,v1,afile), 1)) # Make the file a tuple to have a label
-
-    shuffle(V1) # Randomize list order
-
-    V1_trainLen = math.ceil(len(V1) * Train_Split_Percent)
-    V1_testLen = len(V1) - V1_trainLen
-
-    ############################################################################
     # Get list of V2 files & create list data structure
+    V2, V2_trainLen, V2_testLen = getV(v2, audio, Train_Split_Percent, 2)
+
+    # Create V_Train Wav file
+    V_train = V1[0:V1_trainLen] + V2[0:V2_trainLen]
+    shuffle(V_train)
+    silence = genVTrain(V_train)
+
+    # Create V_Test Wav file
+    # I will linearly index from here b/c the samples are randomized already.
+    V_test= V1[V1_trainLen:V1_trainLen+V1_testLen] + V2[V2_trainLen:V2_trainLen+V2_testLen]
+    shuffle(V_test)
+    genVTest(V_test, silence)
+
+    data = dataPickle(V_train, V_test)
+    printSuccess()
+    return data
+
+def SingleV(v1, audio, Train_Split_Percent):
+    # Get list of V1 files & create list data structure
+    V1, V1_trainLen, V1_testLen = getV(v1, audio, Train_Split_Percent, 1)
+    # Create V_Train Wav file
+    V_train = V1[0:V1_trainLen]
+    shuffle(V_train)
+    silence = genVTrain(V_train)
+
+    # Create V_Test Wav file
+    V_test= V1[V1_trainLen:V1_trainLen+V1_testLen]
+    shuffle(V_test)
+    genVTest(V_test, silence)
+
+    data = dataPickle(V_train, V_test)
+    printSuccess()
+    return data
+
+
+def getV(v, audio, Train_Split_Percent, label):
+    # Get list of V files & create list data structure
 
     # Iterate through each file set in the walk and get a list of all of the audio files
-    V2 = []
-    for dirpath, dirs, files in os.walk(os.path.join(audio, v2)):
+    V = []
+    for dirpath, dirs, files in os.walk(os.path.join(audio, v)):
         for afile in files:
-            V2.append((os.path.join(audio,v2,afile), 2))
+            V.append((os.path.join(audio,v,afile), label)) # Make the file a tuple to have a label
 
-    shuffle(V2) # Randomize list order
+    shuffle(V) # Randomize list order
 
-    # Figure out the trian/test split.
-    V2_trainLen = math.ceil(len(V2) * Train_Split_Percent)
-    V2_testLen = len(V2) - V2_trainLen
-
-    ############################################################################
-    # Create V_Train Wav file
+    V_trainLen = math.ceil(len(V) * Train_Split_Percent)
+    V_testLen = len(V) - V_trainLen
+    return V, V_trainLen, V_testLen
 
 
-    # I will linearly index from here b/c the samples are randomized already.
-    V_train = V1[0:V1_trainLen] + V2[0:V2_trainLen]
-    shuffle(V_train) # Shuffle the two files together
+def genVTrain(V_train):
 
     #Init output label info.
     total_duration = 0.0
     outputPattern = [[0.0,0]] # Start "off"
-
 
     """
     For each audio file:
@@ -66,11 +85,9 @@ def Random_Random(v1, v2, audio):
     end:
     new_total_duration
     """
+
     # Dummy one to instantiate object. Maybe it can be constructed w/o an input,
     # but I don't think it is likely without changing the code.
-
-
-
     Timings = voiceTimings(V_train[0][0])  # Analyze voice data to find start (0) and end (1) timing
 
     V_Train_Wav = AudioSegment.from_wav(V_train[0][0])
@@ -84,8 +101,6 @@ def Random_Random(v1, v2, audio):
     # Find duration of audio sample. make end a 0 to ensure next sample start 0.
     total_duration += V_Train_Wav.duration_seconds
     outputPattern.append([total_duration, 0])
-
-    input("pause")
 
     for clip in V_train[1:len(V_train)]:
         Timings = voiceTimings(clip[0])  # Analyze voice data to find start (0) and end (1) timing
@@ -116,13 +131,16 @@ def Random_Random(v1, v2, audio):
 
     silence = V_Train_Wav - 100  # Creates hopefully a file that is basically silent. -100dB might be overkill
 
+    # Export The Wav files
+    V_Train_Wav.export(os.path.join('.', "V_train.wav"), format="wav")
 
+    return silence
+
+
+def genVTest(V_test, silence):
     ############################################################################
     # Create V_Test Wav file
 
-    # I will linearly index from here b/c the samples are randomized already.
-    V_test= V1[V1_trainLen:V1_trainLen+V1_testLen] + V2[V2_trainLen:V2_trainLen+V2_testLen]
-    shuffle(V_test) # Shuffle the two files together
 
     # Dummy one to instantiate object. Maybe it can be constructed w/o an input,
     # but I don't think it is likely without changing the code.
@@ -130,7 +148,6 @@ def Random_Random(v1, v2, audio):
 
     for clip in V_test[1:len(V_test)]:
         sound = AudioSegment.from_wav(clip[0])  # Index into the tuple to  get the filename
-
 
         V_Test_Wav = V_Test_Wav + sound
 
@@ -144,15 +161,18 @@ def Random_Random(v1, v2, audio):
     print("Len of VTest:")
     print(len(V_test))
 
-
-    # Export The Wav files
-
-    V_Train_Wav.export(os.path.join('.', "V_train.wav"), format="wav")
-
+    # Export The Wav file
     V_Test_Wav.export(os.path.join('.', "V_test.wav"), format="wav")
 
+
+def dataPickle(V_train, V_test):
+    # Store data into pickle file for later if needed.
     data = {"Train": V_train, "Test": V_test}
     pickle.dump( data, open( os.path.join('.', "SimInput", "data_info.p"), "wb" ) )
+    return data
+
+
+def printSuccess():
     Success = """
        _____                             _
       / ____|                           | |
@@ -162,10 +182,10 @@ def Random_Random(v1, v2, audio):
      |_____/ \__,_|\___\___\___||___/___(_)
     """
     print(Success)
-    return data
 
 
 def main():
+    Train_Split_Percent = 0.8
 
     Title = """
                         _ _          _____                           _
@@ -178,8 +198,9 @@ def main():
 
     Enter a Train/Test configuration:
     (1) Random / Random
-    (2) Segments / Random (In Development)
-    (3) Segments / Segments (In Development)
+    (2) Single Voice Randomized
+    (3) Segments / Random (In Development)
+    (4) Segments / Segments (In Development)
     """
     print(Title)
 
@@ -204,17 +225,20 @@ def main():
         if x[0] != AudioFolder:
             print(x[0].replace(AudioFolder,""))
 
-    print("Please pick the two voices you would like to differentiate:")
+    print("Please pick the voice(s) you would like to differentiate:")
 
     V1 = input("Voice 1: ")
-    V2 = input("Voice 2: ")
+    if mode != 2:
+        V2 = input("Voice 2: ")
 
     if mode == 1:
-        Random_Random(V1,V2, AudioFolder)
+        Random_Random(V1,V2, AudioFolder, Train_Split_Percent)
 
     elif mode == 2:
-        print("hi")
+        SingleV(V1, AudioFolder, Train_Split_Percent)
     elif mode == 3:
+        print("hi")
+    elif mode == 4:
         print("hi")
 
 main()
