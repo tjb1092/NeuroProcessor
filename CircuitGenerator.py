7d2bef12683cf777bc469b3245aebc5d792732fc
Tony Bailey <baileytj@mail.uc.edu>
2018-10-05 11:11:53 -0400

    Adding new circuit generators for MNIST STPtoLTPArch

---
+++ CircuitGenerator.py
@@ -0,0 +1,102 @@
+import numpy as np
+from SpiceModifier import SpiceWriter
+
+"""
+# Going to generate a subcircuit that creates a perceptron spiking neural network.
+
+# For now, it will be a single layer.
+
+Initially, it will also be a single big circuit trying each layer individually.
+
+m is the input dimensionality
+n is the number of classes
+
+# Need the following:
+- Input Neuron layer Xni0 - Xnim
+Neuron: input, output
+
+i = vi0 - vim
+o = niout0 - nioutm
+model = liaf
+
+- T-gate layer: Xtgate0 - Xtgatem | Xitgate0 - Xitgatem
+Tgate: input, output, control
+
+i = niout0 - nioutm
+o = pre0 - prem | testpre0 - testprem
+control = train
+model = tgate | itgate
+
+- mxn STRAM Array: Xsyn00 - Xsynmn
+Synapse: pre, post, monitor
+
+i = pre0 - prem
+o = post0 - postn
+state = vsyn00 - vsynmn
+
+- Bidirectional Amplifiers: Xbda0 - Xbdan
+BDA: Vpost, Vnin, vnout, label
+vpost =  post0 - postn
+nin = vnin0 - vninn
+nout = vnout0 - vnoutn
+l = l0 - ln
+
+- Output Neuron Layer: Xno0 - Xnon
+Neuron: input, output
+i = vnin0 - vninn
+o = vnout0 - vnoutn
+"""
+
+
+
+def main():
+
+    content = []
+
+    num_features = 9
+    num_classes = 8
+
+    testMode = 5
+    # First, I also need to add statements importing all of the models.
+
+    # I can totally make this in one double loop. Just for now, I want to build it
+    #up
+
+    # Generate the input neuron layer:
+    for m in range(num_features):
+        new_string = "Xni{} vi{} niout{} liaf \n".format(m, m, m)
+        content.append(new_string)
+
+
+    if testMode > 1:
+        # Generate T-gate layers
+        for m in range(num_features):
+            new_string = "Xtgate{} niout{} pre{} train tgate \n".format(m, m, m)
+            content.append(new_string)
+            new_string = "Xitgate{} niout{} testpre{} train itgate \n".format(m, m, m)
+            content.append(new_string)
+
+    if testMode > 2:
+        # Generate STRAM array
+        for m in range(num_features):
+            for n in range(num_classes):
+                new_string = "Xsyn{}{} pre{} post{} mon{}{} STRAM \n".format(m, n, m, n, m, n)
+                content.append(new_string)
+
+    if testMode > 3:
+        for n in range(num_classes):
+            new_string = "Xbda{} post{} vnin{} vnout{} l{} BDA \n".format(n, n, n, n, n)
+            content.append(new_string)
+
+    if testMode > 4:
+        for n in range(num_classes):
+            new_string = "Xno{} vnin{} vnout{} liaf \n".format(n, n, n)
+            content.append(new_string)
+
+
+    Ckt_element_fn = "DigitFiles/GeneratedCkt.sp"
+    SpiceWriter(content, Ckt_element_fn)
+
+
+if __name__ == "__main__":
+    main()
---
+++ MNIST_Loader.py
@@ -0,0 +1,188 @@
+import numpy as np
+from mnist import MNIST
+from SpiceModifier import SpiceWriter
+mndata = MNIST('/home/tonyjb/MNIST')
+
+# Some global vars
+vdd = 3.3  # Voltages will be relative to the process node voltage
+#feature_length = 784
+#num_classes = 10
+feature_length = 9
+num_classes = 8
+
+Toff = Ton = 0.0005  # Duration of on vs. off time.
+T_rf = 1e-7  # Rise and fall time
+# To get the training set sent through the network in the time frame the STPRAM
+# will understand, I'll probably need to reduce this to .1ms - > training of 6sec.
+
+
+
+def encode_digits(images, labels, initial_duration):
+
+    # Scale images:
+    im_scaled = np.zeros((len(images),feature_length))
+    for istep, im in enumerate(images):
+        im_scaled[istep] = np.multiply((vdd/255.0),im)
+
+    # Now encode MNIST digits into 784 voltage sources w/ PWL functions
+    # Input Struct: [pixel_num: [time, val]] - List of lists
+    # Output Struct: [class_num: [time, val]]
+
+    inputPattern = []
+    outputPattern = []
+    total_duration = initial_duration
+
+    # Instantiate input list of numpy arrays:
+    # a list of lists ate all of my memory. preallocation should be faster anywho.
+
+    inputPattern = np.zeros((feature_length,len(im_scaled)*4+1,2))
+    outputPattern = np.zeros((num_classes,len(im_scaled)*4+1,2))
+    print(inputPattern.shape)
+
+    # Implicitly define the end of one sample as the beginning of the next sample.
+    for istep, im in enumerate(im_scaled):
+        if istep % 1000 == 0:
+            print(istep, total_duration)
+        # Two loops: One for inputs and one for outputs.
+
+        # Input loop: Go through each pixel in the image.
+        # Transfer that array of pixels into one unit of the time-series input
+
+        for pstep, pix in enumerate(im):
+            # _
+            temp_duration = total_duration
+
+            temp_duration += Toff
+            inputPattern[pstep][istep*4+1][:] = [temp_duration, 0.0]
+            # _/
+            temp_duration += T_rf
+            inputPattern[pstep][istep*4+2][:] = [temp_duration, pix]
+            # _/--
+            temp_duration += Ton
+            inputPattern[pstep][istep*4+3][:] = [temp_duration, pix]
+            # _/--\
+            temp_duration += T_rf
+            inputPattern[pstep][istep*4+4][:] = [temp_duration, 0.0]
+
+        total_duration = temp_duration  # Make last temp the total_duration
+        # Output loop: loop over one input array. change to label where
+        # appropriate for the correct label array. Leave rest as zeros.
+
+        tmp = inputPattern[0] # Pull out first pixel's timing sequence.
+
+        label = labels[istep]  # pull out the label for this sample.
+
+        for c in range(num_classes):
+            # Generate the timing for each output label class for this sample.
+
+            if c == label:
+                # Make the "correct" label output a label voltage.
+                # Might need to flip this based on my logic definition.
+                l_V = vdd
+            else:
+                l_V = 0.0
+
+            # Use sample index to get the proper index from tmp.
+            outputPattern[c][istep*4+1][:] = [tmp[istep*4+1][0], 0.0]
+            outputPattern[c][istep*4+2][:] = [tmp[istep*4+2][0], l_V]
+            outputPattern[c][istep*4+3][:] = [tmp[istep*4+3][0], l_V]
+            outputPattern[c][istep*4+4][:] = [tmp[istep*4+4][0], 0.0]
+
+    print("total_duration: {} s".format(total_duration))
+
+
+
+    return(inputPattern, outputPattern, total_duration)
+
+def test_loader():
+    # Some simple test data to try out
+    samples = np.array(((0, 255, 0, 255, 0, 255, 0, 255, 0),
+                        (255, 0, 255, 0, 255, 0, 255, 0, 255),
+                        (0,  0, 255, 0, 0, 255, 0, 0, 255),
+                        (255, 255, 255, 0, 0, 0, 0, 0, 0),
+                        (0, 0, 0, 255, 255, 255, 0, 0, 0),
+                        (0, 0, 0, 0, 0, 0, 255, 255, 255),
+                        (255, 0, 0, 255, 0, 0, 255, 0, 0),
+                        (0, 255, 0, 0, 255, 0, 0, 255, 0)))
+    labels = np.array(((0),
+                        (1),
+                        (2),
+                        (3),
+                        (4),
+                        (5),
+                        (6),
+                        (7)))
+    print(samples.shape, labels.shape)
+    return samples, labels
+
+
+def Generate_IO_Voltages(content, pattern, name, T_T):
+    p_shape = pattern.shape
+    num_sources = p_shape[0]  # Index the first term in shape.
+
+    source_counter = 0
+
+
+    for source in range(num_sources):
+
+        if T_T == 1:
+            # Training Source
+            vi = "v{}{}".format(name, source)
+            vo = "v{}{}t".format(name, source)
+            t_t = "tr"
+        else:
+            # Testing Source
+            vi = "v{}{}t".format(name, source)
+            vo = "0"
+            t_t = "te"
+
+
+        #For each source, append the correct information per sample to the string.
+        newString = "V{}{}{} {} {} pwl(".format(name, source, t_t,  vi, vo)
+
+        for row in pattern[source]:
+            newString += " {0:.7f} {1:.3f}".format(row[0], row[1])
+
+        newString += ")\n"
+        content.append(newString) #Override template w/ new command
+
+    # Write out newly modified file into the main directory
+
+
+
+
+
+def main():
+
+
+    isMNIST = False
+
+    if isMNIST:
+        # Loads MNIST into python arrays. Not numpy here.
+        im_train, label_train = mndata.load_training()
+        im_test, label_test = mndata.load_testing()
+    else:
+        im_train, label_train = test_loader()
+        im_test, label_test = test_loader()
+
+
+    print("Generating voltages for training:")
+    input_train, output_train, train_duration = encode_digits(im_train, label_train, 0)
+
+    # Add a "isTraining" signal based on train_duration!
+
+    print("Generating voltages for testing:")
+    input_test, output_test, total_duration = encode_digits(im_test, label_test, train_duration)
+
+    content = []
+    Generate_IO_Voltages(content, input_train, "i", 1)
+    Generate_IO_Voltages(content, output_train, "l", 1)
+    Generate_IO_Voltages(content, input_test, "i", 0)
+    Generate_IO_Voltages(content, output_test, "l", 0)
+
+
+    voltage_element_fn = "DigitFiles/VoltageSources.sp"
+    SpiceWriter(content, voltage_element_fn)
+
+if __name__ == "__main__":
+    main()
